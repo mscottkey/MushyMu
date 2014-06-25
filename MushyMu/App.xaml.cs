47d5a965d6c1e1768591b63143d7f56c0a30a339
mscottkey <mscottkey@gmail.com>
2014-06-24 19:04:50 -0700

    fixed connect functionality

---
+++ MushyMu/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using GalaSoft.MvvmLight.Threading;
+using System.Windows.Media;
+using Elysium;
+
+
+namespace MushyMu
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : System.Windows.Application
+    {
+        
+        static App()
+        {
+            DispatcherHelper.Initialize();
+        }
+
+        private void StartupHandler(object sender, System.Windows.StartupEventArgs e)
+        {
+            Elysium.Manager.Apply(this, Elysium.Theme.Dark, Brushes.DarkRed, Brushes.Black);
+        }
+    }
+}
---
+++ MushyMu/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using MushyMu.ViewModel;
+
+namespace MushyMu
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Elysium.Controls.Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the MainWindow class.
+        /// </summary>
+        public MainWindow()
+        {
+            Closing += (s, e) => ViewModelLocator.Cleanup();
+        }
+    }
+}
